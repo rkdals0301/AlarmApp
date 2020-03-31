d81db25afda2215ee0c36ab9408edbf628946b8d
LeeGangMin <rkdals0301@naver.com>
2020-03-31 14:04:48 +0900

    알람 Fragment 구현

--- app/src/main/java/com/gangmin/alarm/activity/MainActivity.kt
+++ app/src/main/java/com/gangmin/alarm/activity/MainActivity.kt
@@ -3,11 +3,16 @@ package com.gangmin.alarm.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gangmin.alarm.R
+import com.gangmin.alarm.fragment.AlarmFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.view, AlarmFragment())
+            .commit()
     }
 }
