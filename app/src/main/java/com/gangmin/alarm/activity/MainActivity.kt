814079dc177b4166ee00920ec253b48a0091b51d
rkdals0301 <rkdals0301@naver.com>
2020-04-03 03:16:18 +0900

    1. ProGuard 코드 난독화 / 최적화 설정 2. RecyclerView 구현 진행중.. 3. minSdkVersion 21(Lollipop)으로 변경

--- app/src/main/java/com/gangmin/alarm/activity/MainActivity.kt
+++ app/src/main/java/com/gangmin/alarm/activity/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.beginTransaction()
-            .replace(R.id.view, AlarmFragment())
+            .replace(R.id.frame_layout, AlarmFragment())
             .commit()
     }
 }
